1fd0cc843591ece7a900699445b624068078cdd8
virajcz <virajchhajed21@gmail.com>
2024-10-07 15:15:32 -0500

    feat(async): implement test async I2C communication for SLF3S

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,17 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::i2c::{Config, I2c};
 use embassy_stm32::time::Hertz;
-use embassy_time::{block_for, Duration};
+use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 const ADDRESS: u8 = 0x08;
 const PROD_IDENTIFY_1: [u8; 2] = [0x36, 0x08];
+
+bind_interrupts!(struct Irqs {
+    I2C2_EV => i2c::EventInterruptHandler<peripherals::I2C2>;
+    I2C2_ER => i2c::ErrorInterruptHandler<peripherals::I2C2>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -21,19 +27,29 @@ async fn main(_spawner: Spawner) {
     config.scl_pullup = true;
     config.sda_pullup = true;
 
-    let mut i2c = I2c::new_blocking(p.I2C2, p.PB10, p.PB11, Hertz(100_000), config);
+    let mut i2c = I2c::new(
+        p.I2C2,
+        p.PB10,
+        p.PB11,
+        Irqs,
+        p.DMA1_CH7,
+        p.DMA1_CH2,
+        Hertz(100_000),
+        config,
+    );
 
     let mut data = [0u8; 18];
 
-    block_for(Duration::from_millis(100));
+    Timer::after_millis(100).await;
 
-    match i2c.blocking_write(ADDRESS, &PROD_IDENTIFY_1) {
+    match i2c.write(ADDRESS, &PROD_IDENTIFY_1).await {
         Ok(()) => info!("First command sent successfully"),
         Err(e) => error!("Error sending first command: {:?}", e),
     }
-    block_for(Duration::from_millis(100));
 
-    match i2c.blocking_read(ADDRESS, &mut data) {
+    Timer::after_millis(100).await;
+
+    match i2c.read(ADDRESS, &mut data).await {
         Ok(()) => info!("Data read successfully: {:?}", data),
         Err(e) => error!("Error reading data: {:?}", e),
     }
