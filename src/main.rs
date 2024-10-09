f4ef4d9c457813dba6f310fd05c3ea643642b837
virajcz <virajchhajed21@gmail.com>
2024-10-08 19:50:03 -0500

    feat(ExplorIR-M-E-100): read sensor info for CO2 sensor using USART DMA
    - Configure USART for communication with ExplorIR-M-E-100 sensor
    - Implement command sending to request sensor information
    - Add logic to read and parse sensor response
    - Display raw response bytes for debugging and verification

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::usart::{Config, DataBits, Parity, StopBits, Uart};
 use embassy_stm32::{bind_interrupts, peripherals, usart};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
@@ -17,7 +18,7 @@ async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
     let mut config = Config::default();
-    config.baudrate = 19200;
+    config.baudrate = 9600;
     config.parity = Parity::ParityNone;
     config.stop_bits = StopBits::STOP1;
     config.data_bits = DataBits::DataBits8;
@@ -25,9 +26,11 @@ async fn main(_spawner: Spawner) {
     let mut usart =
         Uart::new(p.USART3, p.PD9, p.PD8, Irqs, p.DMA1_CH3, p.DMA1_CH1, config).unwrap();
 
-    let cmd = [0xFF, 0xFE, 0x02, 0x02, 0x01]; //this is the command to read the serial no
+    Timer::after_secs(2).await;
 
-    match usart.write(&cmd).await {
+    let cmd = b"Y\r\n"; //this is the command to read the serial no
+
+    match usart.write(cmd).await {
         Ok(_) => {
             info!("Command sent successfully");
         }
@@ -36,25 +39,23 @@ async fn main(_spawner: Spawner) {
         }
     }
 
-    let mut response = [0u8; 20];
+    let mut response = [0u8; 47];
     let mut index = 0;
+
     while index < response.len() {
         match usart.read(&mut response[index..index + 1]).await {
             Ok(_) => {
                 index += 1;
-                if index >= 3 && response[2] as usize + 3 == index {
-                    break;
-                }
             }
-            Err(_) => error!("error while reading"),
+            Err(_) => {
+                error!("Error while reading");
+                break;
+            }
         }
     }
 
-    if index >= 6 && response[0] == 0xFF && response[1] == 0xFA {
-        let mut serial_number = [0u8; 15];
-        serial_number.copy_from_slice(&response[3..18]);
-        info!("{:02X}", serial_number)
-    } else {
-        error!("error displaying the serial number")
-    }
+    info!(
+        "Raw response bytes: {:?}",
+        core::str::from_utf8(&response[..index]).unwrap_or("<invalid UTF-8>")
+    );
 }
