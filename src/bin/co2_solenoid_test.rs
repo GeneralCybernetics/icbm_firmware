a0f2626b18da11762a96178d4eae07914d1e2562
virajcz <virajchhajed21@gmail.com>
2024-10-21 18:03:31 -0500

    chore: restructure project layout and improve module organization
    - Create empty main.rs for future implementation
    - Add lib.rs to expose drivers as a public module
    - Move testing code to bin/co2_solenoid_test.rs
    
    Note: Improves project structure for better maintainability and testing.

--- src/bin/co2_solenoid_test.rs
+++ src/bin/co2_solenoid_test.rs
@@ -3,6 +3,9 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Speed};
+use embassy_time::Timer;
+use icbm_firmware::drivers::co2_solenoid::{Co2Solenoid, Co2State};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
