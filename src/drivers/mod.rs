f26919e308e49e39c67d828150d22a549b542f05
virajcz <virajchhajed21@gmail.com>
2024-11-05 18:35:39 -0600

    chore(drivers): update mod.rs and main.rs

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -3,3 +3,4 @@ pub mod co2_solenoid;
 pub mod explorir_m_e_100;
 pub mod scd41;
 pub mod slf3s;
+pub mod drv8873;
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use icbm_firmware::drivers::co2_solenoid;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
